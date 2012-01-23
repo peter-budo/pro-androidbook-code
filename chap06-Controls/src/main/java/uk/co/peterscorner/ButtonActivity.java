19640496a3262075154bac4ca2ef73510be7aec4
Peter <Peter@Peter-Mikloskos-MacBook-Pro.local>
2012-01-23 11:19:58 +0000

    - Simple example of Buttons

---
+++ chap06-Controls/src/main/java/uk/co/peterscorner/ButtonActivity.java
@@ -0,0 +1,13 @@
+package uk.co.peterscorner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ButtonActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttons);
+    }
+}
