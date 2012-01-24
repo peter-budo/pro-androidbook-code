c686f4ce8723afad8aee2b7996e65677040cb573
Peter <Peter@Peter-Mikloskos-MacBook-Pro.local>
2012-01-24 16:51:52 +0000

    - Simple example of Spinner

---
+++ chap06-Controls/src/main/java/uk/co/peterscorner/SpinnerActivity.java
@@ -0,0 +1,23 @@
+package uk.co.peterscorner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SpinnerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spinner);
+        
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.planets, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+}
