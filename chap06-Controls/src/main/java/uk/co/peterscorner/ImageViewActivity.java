48346fea07e2b45f7ac1bbba5b22255de4e525ab
Peter <Peter@Peter-Mikloskos-MacBook-Pro.local>
2012-01-24 12:02:29 +0000

    - Simple example of ImageView

--- chap06-Controls/src/main/java/uk/co/peterscorner/ImageViewActivity.java
+++ chap06-Controls/src/main/java/uk/co/peterscorner/ImageViewActivity.java
@@ -1,25 +1,29 @@
 package uk.co.peterscorner;
 
 import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
 
 public class ImageViewActivity extends Activity {
-
-    private static String TAG = "chap06-Controls";
-
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+        setContentView(R.layout.imageview);
 
+        ImageView imgView = (ImageView)findViewById(R.id.image3);
+        
+        imgView.setImageResource(R.drawable.icon);
+        
+        imgView.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.manatee02));
+
+        /* For these last 2 images to work, you need to provide link to existing images on your device
+        * and either test it with device connected with IDE or manually upload application
+        * on memory card and then install*/
+        imgView.setImageDrawable(Drawable.createFromPath("/mnt/sdcard/dave2.jpg"));
+        imgView.setImageURI(Uri.parse("file://mnt/sdcard/dave2.jpg"));
+    }
 }
 
