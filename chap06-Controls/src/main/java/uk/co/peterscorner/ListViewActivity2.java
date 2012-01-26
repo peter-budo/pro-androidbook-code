e25ce160380defd80de582af91223fd4ab7a8a83
Peter <Peter@Peter-Mikloskos-MacBook-Pro.local>
2012-01-26 18:23:51 +0000

    - ListViewActivity2 example of click action with launching Contacts app -ListViewActivity3 example of adding checkboxes to list, however at the moment broken when getting messages on Log after click

---
+++ chap06-Controls/src/main/java/uk/co/peterscorner/ListViewActivity2.java
@@ -0,0 +1,42 @@
+package uk.co.peterscorner;
+
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+public class ListViewActivity2 extends ListViewActivity implements AdapterView.OnItemClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ListView lv = getListView();
+
+        Cursor c = managedQuery(Contacts.CONTENT_URI, null, null, null, Contacts.DISPLAY_NAME);
+        String[] cols = new String[]{Contacts.DISPLAY_NAME};
+        int[] views = new int[] {android.R.id.text1};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                c, cols, views);
+        this.setListAdapter(adapter);
+        lv.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> adapterView, View target, int position, long id) {
+        Log.v("ListViewActivity2 ", "in onItemClick with " + ((TextView)target).getText() +
+        ". Position = " + position + ". Id = " + id);
+        Uri selectedPerson = ContentUris.withAppendedId(Contacts.CONTENT_URI, id);
+        Intent intent = new Intent(Intent.ACTION_VIEW, selectedPerson);
+        startActivity(intent);
+    }
+}
---
+++ chap06-Controls/src/main/java/uk/co/peterscorner/ListViewActivity3.java
@@ -0,0 +1,59 @@
+package uk.co.peterscorner;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class ListViewActivity3 extends ListActivity {
+    private static final String TAG = "ListViewActivity3";
+    private ListView lv = null;
+    private Cursor cursor = null;
+    private int idCol = -1;
+    private int nameCol = -1;
+    private int notesCol = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+
+        lv = getListView();
+
+        cursor = managedQuery(Contacts.CONTENT_URI, null, null, null, Contacts.DISPLAY_NAME);
+        String[] cols = new String[]{Contacts.DISPLAY_NAME};
+        idCol = cursor.getColumnIndex(Contacts._ID);
+        nameCol = cursor.getColumnIndex(Contacts.DISPLAY_NAME);
+        //this is replacement for deprecated Contacts.People.NOTE
+        notesCol = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Note.NOTE);
+
+        int[] views = new int[]{android.R.id.text1};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this
+                , android.R.layout.simple_list_item_multiple_choice
+                , cursor, cols, views);
+        
+        this.setListAdapter(adapter);
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+    
+    public void doClick(View view){
+        int count = lv.getCount();
+        SparseBooleanArray viewItems = lv.getCheckedItemPositions();
+        for(int i=0; i < count; i++){
+            if(viewItems.get(i)){
+                cursor.moveToPosition(i);
+                long id = cursor.getLong(idCol);
+                String name = cursor.getString(nameCol);
+                String notes = cursor.getString(notesCol);
+                Log.v(TAG, name + " is checked. Notes= " + notes + ". Position = " + i + ". Id = " + id);
+            }
+        }
+    }
+}
