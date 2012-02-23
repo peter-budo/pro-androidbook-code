0d72a6ed915624f9bdbd91affc2a3b2f414eff6d
Peter <Peter@Peter-Mikloskos-MacBook-Pro.local>
2012-02-23 11:47:29 +0000

    - GridView simple example just listing contacts in default GridView setup

---
+++ chap06-Controls/src/main/java/uk/co/peterscorner/GridViewActivity.java
@@ -0,0 +1,27 @@
+package uk.co.peterscorner;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.SimpleCursorAdapter;
+
+import static android.provider.ContactsContract.Contacts;
+
+public class GridViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.gridview);
+        
+        GridView gv = (GridView)findViewById(R.id.gridview);
+        Cursor c = managedQuery(Contacts.CONTENT_URI, null, null, null, Contacts.DISPLAY_NAME);
+        
+        String[] cols = new String[] {Contacts.DISPLAY_NAME};
+        int[] views = new int[] {android.R.id.text1};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, c, cols, views);
+        gv.setAdapter(adapter);
+    }
+}
